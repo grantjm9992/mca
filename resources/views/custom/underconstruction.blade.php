c96e06754f7852284d91dfe0900ef5257a457d19
Grant MacDonald <phisoluciones.es@gmail.com>
2020-08-12 16:56:52 +0200

    Change over file structure

---
+++ resources/views/custom/underconstruction.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Chases Places Property Management</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	
+	
+	<!-- Font -->
+	
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700%7CPoppins:400,500" rel="stylesheet">
+	
+	<!-- Stylesheets -->
+	
+	<link href="assets/custom/common-css/bootstrap.css" rel="stylesheet">
+	
+	
+	<link href="assets/custom/common-css/ionicons.css" rel="stylesheet">
+	
+	
+	<link rel="stylesheet" href="assets/custom/common-css/jquery.classycountdown.css" />
+		
+	<link href="assets/custom/01-comming-soon/css/styles.css?v=4" rel="stylesheet">
+	
+	<link href="assets/custom/01-comming-soon/css/responsive.css" rel="stylesheet">
+	
+</head>
+<body>
+	
+	<div class="main-area" style="background-color: black;">
+		<div class="container full-height position-static">
+			
+			<section class="left-section full-height">
+		
+				<a class="logo" href="#"><img src="assets/custom/images/Chases-Places-Logo-Final-PNG-Trans-Bkg.png" alt="Logo"></a>
+				
+				<div class="display-table">
+					<div class="display-table-cell">
+						<div class="main-content">
+							<h1 class="title"><b>Coming Soon</b></h1>
+							<p>
+								Chases Places is a new kind of property management company, offering a full service operation, created specifically to give homeowners the ability to maximise their short-term & long-term rental income without any hassle
+							</p>
+							<ul style="padding-left: 40px; margin-top: 20px;">
+								<li class="list-item">
+								Expert Rental Services
+								</li>
+								<li class="list-item">
+								Property Inspections
+								</li>
+								<li class="list-item">
+								Cleaning & Maintenance
+								</li>
+								<li class="list-item">
+								Refurbishments
+								</li>
+								<li class="list-item">
+								Owners Online Dashboard
+								</li>
+								<li class="list-item">
+								Affordable Payment plans
+								</li>
+								<li class="list-item">
+								Consistent updates 
+								</li>
+							</ul>
+
+							<div class="email-input-area">
+								<form method="post" action="Home.register">
+                                    @csrf()
+									<input class="email-input" name="email" type="text" placeholder="Enter your email">
+									<button class="submit-btn" style="background-color: #62a5c2;" name="submit" type="submit"><b>NOTIFY US</b></button>
+								</form>
+							</div><!-- email-input-area -->
+							
+							<p class="post-desc">Sign up to receive our Brochure and Prices for property management!</p>
+						</div><!-- main-content -->
+					</div><!-- display-table-cell -->
+				</div><!-- display-table -->
+				
+				<ul class="footer-icons">
+					<li>Stay in touch : </li>
+					<li><a href="https://facebook.com/chasesplaces/" target="_blank"><i class="ion-social-facebook"></i></a></li>
+					<li><a href="https://instagram.com/chases_places_pm/" target="_blank"><i class="ion-social-instagram-outline"></i></a></li>
+				</ul>
+		
+			</section><!-- left-section -->
+		
+			<section class="right-section" style="background-image: url(assets/custom/images/Location-1.jpg)">
+			
+				<div class="display-table center-text">
+					<div class="display-table-cell">
+						
+						
+						<div id="rounded-countdown">
+							<div class="countdown" data-remaining-sec="2000000"></div>
+						</div>
+						
+					</div><!-- display-table-cell -->
+				</div><!-- display-table -->
+				
+			</section><!-- right-section -->
+		
+		</div><!-- container -->
+	</div><!-- main-area -->
+	
+	<!-- SCIPTS -->
+	
+	<script src="assets/custom/common-js/jquery-3.1.1.min.js"></script>
+	
+	<script src="assets/custom/common-js/tether.min.js"></script>
+	
+	<script src="assets/custom/common-js/bootstrap.js"></script>
+	
+	<script src="assets/custom/common-js/jquery.classycountdown.js"></script>
+	
+	<script src="assets/custom/common-js/jquery.knob.js"></script>
+	
+	<script src="assets/custom/common-js/jquery.throttle.js"></script>
+	
+	<script src="assets/custom/common-js/scripts.js"></script>
+	
+</body>
+</html>
